abbf2c40a579b5c7920dccd017111d78d01e5a7a
carlos_adao <jose.carlos.adao@hotmail.com>
2020-11-13 07:42:33 -0300

    finish jetpack mvvm

--- app/src/main/java/com/example/aula31mvvm/MainActivity.kt
+++ app/src/main/java/com/example/aula31mvvm/MainActivity.kt
@@ -3,22 +3,64 @@ package com.example.aula31mvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.lifecycle.LiveData
+
 
 class MainActivity : AppCompatActivity() {
-    private  val model: MainViewModel by viewModels()
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        model.soma.observe(this, Observer {
-            tvRes.text = it.toString()
-        })
-
-        fbAdd.setOnClickListener {
-           model.doSun(1)
+        setContent {
+            MaterialTheme {
+                myApp()
+            }
         }
+    }
+
+    @Composable
+    fun myApp(){
+        Scaffold(
+            topBar = {},
+            bodyContent = {
+                contador(viewModel.soma)
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {viewModel.doSun(1)}){
+                    Icon(Icons.Filled.Add)
+                }
+            }
+        )
+    }
+
+    @Composable
+    fun contador(somaData:LiveData<Int>){
+        val soma by somaData.observeAsState(0)
+        Column(
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(soma.toString())
+        }
+
     }
 }
--- app/src/main/java/com/example/aula31mvvm/MainViewModel.kt
+++ app/src/main/java/com/example/aula31mvvm/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.aula31mvvm
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
